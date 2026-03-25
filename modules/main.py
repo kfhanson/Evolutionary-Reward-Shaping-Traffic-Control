d0651d818c9b9f356d72babf28f82d68321b73f7
kfhanson <kfhanson.2508@gmail.com>
2026-03-26 02:05:57 +0700

    Typo Update

--- modules/main.py
+++ modules/main.py
@@ -10,12 +10,12 @@ def main():
     print("======================================================")
 
     if not os.path.exists(csv_file):
-        print("\n[PHASE 1] Generating offline traffic data using Rule-based Policy...")
+        print("\nGenerating offline traffic data using Rule-based Policy")
         sumo_data.run_sumo_and_log_data(num_simulation_steps=30000, data_collection_policy_name="rule_based")
     else:
-        print(f"\n[PHASE 1] Found existing data '{csv_file}'. Skipping collection.")
+        print(f"\nFound existing data '{csv_file}'. Skipping collection.")
 
-    print("\n[PHASE 2 & 3] Evolving LSTM-DQN Reward Weights with NSGA-II...")
+    print("\nEvolving LSTM-DQN Reward Weights with NSGA-II...")
     nsga2_optimization.run_nsga2(offline_data_path=csv_file, pop_size=30, n_gen=20)
 
     print("\nCOMPLETED")
